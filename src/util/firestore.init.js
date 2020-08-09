1327a2861168bd603b72ed91620c3d9653278178
Antariksh C <antarikshc@gmail.com>
2020-08-10 02:42:07 +0530

    chore: Init Firestore

---
+++ src/util/firestore.init.js
@@ -0,0 +1,27 @@
+import admin from 'firebase-admin';
+
+const serviceAccount = require('../../appointex-firebase-adminsdk.json');
+
+let db;
+
+function getDatabase() {
+  try {
+    if (db) {
+      return db;
+    }
+
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: 'https://the-appointex-project.firebaseio.com',
+    });
+
+    db = admin.firestore();
+
+    return db;
+  } catch (e) {
+    console.error(e.stack);
+    return null;
+  }
+}
+
+module.exports = { getDatabase };
