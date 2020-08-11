8516f9befd3f1a145699230797eb5c17aea5bf67
Antariksh C <antarikshc@gmail.com>
2020-08-11 17:03:39 +0530

    feat: checkEventCollision covers all posibilities

--- src/data/events.dao.js
+++ src/data/events.dao.js
@@ -44,6 +44,13 @@ export default class EventsDao {
     return events;
   }
 
+  /**
+   * Queries events which might collide with the timings given
+   * @param {number} startTime in millis
+   * @param {number} endTime in millis
+   * @param {number} timeZoneOffset in minutes
+   * @returns {Boolean} to indicate whether collision occurs
+   */
   static async checkEventCollision(startTime, endTime, timeZoneOffset) {
     try {
       let startInMillis = startTime;
@@ -57,21 +64,50 @@ export default class EventsDao {
       const start = new Date(startInMillis);
       const end = new Date(endInMillis);
 
+      // There are 4 possibilities
+      // Intended booking:           |------------|
+      // 4 Posibilities:
+      //                  |______1____| |__2__| |______3______|
+      //                         |_________4__________|
+
+      // Covers 1st & 2nd posibilities
       const startCollisonQuery = await db.collection('events')
         .where('startTime', '>=', start)
         .where('startTime', '<', end)
         .get();
 
+      // Covers 2nd & 3rd posibilities
       const endCollisonQuery = await db.collection('events')
         .where('endTime', '>', start)
         .where('endTime', '<=', end)
         .get();
 
-      if (startCollisonQuery.docs.length > 0 || endCollisonQuery.docs.length > 0) {
+      // Covers 4th posibility
+      // Here we assume that booking cannot overlap 2 days,
+      // so lower limit is start of the day
+      const startOfDay = getStartOfDay(start);
+      const startEndCollisonQuery = await db.collection('events')
+        .where('startTime', '<', start)
+        .where('startTime', '>=', startOfDay)
+        .get();
+
+      const startEndCollisonDocs = startEndCollisonQuery.docs.filter((doc) => {
+        if (doc.data().endTime.toMillis() > endInMillis) {
+          return true;
+        }
+        return false;
+      });
+
+      if (
+        startCollisonQuery.docs.length > 0 ||
+        endCollisonQuery.docs.length > 0 ||
+        startEndCollisonDocs.docs.length > 0
+      ) {
         return true;
       }
     } catch (e) {
       console.error(`checkEventCollision : ${e.stack}`);
+      return true;
     }
 
     return false;
