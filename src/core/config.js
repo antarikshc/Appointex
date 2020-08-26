84a6f643269ca69e5d4a0824a7b1fce211d2f892
Antariksh C <antarikshc@gmail.com>
2020-08-27 02:03:42 +0530

    chore: Allow config to be set by env params

--- src/core/config.js
+++ src/core/config.js
@@ -1,10 +1,12 @@
+import 'dotenv/config';
+
 /**
  * Returns the default config for bounds
  * All fields are in Minutes
  */
 module.exports = {
-  clockIn: 9 * 60,
-  clockOut: 17 * 60,
-  duration: 30,
-  timeZoneOffset: 5.5 * 60, // India = +5:30
+  clockIn: process.env.CONFIG_CLOCK_IN || 9 * 60,
+  clockOut: process.env.CONFIG_CLOCK_OUT || 17 * 60,
+  duration: process.env.CONFIG_DURATION || 30,
+  timeZoneOffset: process.env.CONFIG_TIME_ZONE || 5.5 * 60, // India = +5:30
 };
